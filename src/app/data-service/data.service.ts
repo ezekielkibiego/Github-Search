273e30629bb3f7314966744b8bddc3fadeabee8f
ezekielkibiego <ezekiel.nyambane@student.moringaschool.com>
2021-10-16 16:33:47 +0300

    add dataservice declarations

--- src/app/data-service/data.service.ts
+++ src/app/data-service/data.service.ts
@@ -1,9 +1,14 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { User } from '../users/user';
+import { Repos } from '../users/repos';
 
 @Injectable({
   providedIn: 'root'
 })
 export class DataService {
+  userGottenDetails!: User;
+  userRepositories!: Repos;
 
   constructor() { }
 }
