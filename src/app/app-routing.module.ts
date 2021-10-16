2ce7e9909276ca18907fa59ff9621d0a9e6fdd89
ezekielkibiego <ezekiel.nyambane@student.moringaschool.com>
2021-10-16 14:33:26 +0300

    generate classes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from './home/home.component';
-import { NavbarComponent } from './navbar/navbar.component';
+import { UserComponent } from './user/user.component';
+import { ReposComponent } from './repos/repos.component';
 
 const routes: Routes = [
   { path: 'home', component: HomeComponent},
-  { path: 'navbar', component: NavbarComponent},
+  { path: 'user', component: UserComponent},
+  { path: 'repos', component: ReposComponent},
 ];
 
 @NgModule({
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,8 +3,9 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
 };
+// 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    personal access token
 
 /*
  * For easier debugging in development mode, you can import the following file
