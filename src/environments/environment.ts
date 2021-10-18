906fe6e34519cfa07fdb18ec41c4ae94fd3e9913
ezekielkibiego <ezekiel.nyambane@student.moringaschool.com>
2021-10-19 01:08:32 +0300

    edited README.md file

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,7 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: true,
+  production: false,
   
   apiKey: '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',
   apiUrl: 'https://api.github.com/users'
